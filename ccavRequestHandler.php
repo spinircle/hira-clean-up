2369026f5c409b4706720a9a2f012f61cb6dc911
karthikeyankkn <connecttokkn@hotmail.com>
2018-03-17 11:24:15 +0530

    modified dataForm page

--- ccavRequestHandler.php
+++ ccavRequestHandler.php
@@ -21,7 +21,7 @@
 	$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
 
 ?>
-<form method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
+<form method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
 <?php
 echo "<input type=hidden name=encRequest value=$encrypted_data>";
 echo "<input type=hidden name=access_code value=$access_code>";
